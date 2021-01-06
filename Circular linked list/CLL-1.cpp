77b85444934a8c8d0152294d9b5313bd7f91757a
Adarsh Kesarwani <50302943+adarsxh@users.noreply.github.com>
2021-01-07 00:27:33 +0530

    Add files via upload

---
+++ Circular linked list/CLL-1.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+	int data;
+	node *next;
+	node(int x){
+		data=x;
+		next = NULL;
+	}
+};
+void printlist(node *head){
+	if(head==NULL)
+		return ;
+	cout<<(head->data)<<" ";
+	for(node *p=head->next;p!=head;p=p->next){
+		cout<<(p->data)<<" ";
+	}
+}
+
+void printlist2(node *head){
+	if(head==NULL)
+		return ;
+	node *p=head;
+	do{
+		cout<<(p->data)<<" ";
+		p=p->next;
+	}
+	while(p!=head);
+}
+
+int main(){
+	node *head=new node(10);
+	head->next = new node(20);
+	head->next->next = new node(30);
+	head->next->next->next = new node(40);
+	head->next->next->next->next=head;
+	printlist2(head);
+}
